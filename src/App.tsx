28285d56e78b94db0b5d0c1bd1958358f58a5127
kurehajime <xiidec@gmail.com>
2025-08-17 19:41:03 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GameElement from './components/GameElement'
 
 export default function App() {
   return (
-    <div style={{ display: 'grid', alignItems: 'start', justifyItems: 'center', paddingTop: 4 }}>
+    <div style={{ display: 'grid', alignItems: 'start', justifyItems: 'center', padding: 8, width: '100%' }}>
       <GameElement />
     </div>
   )
